7d4d4e4817f5ba92b5aae091854188ed27f58f82
cgombauld <clement.gombauld@cpexterne.org>
2022-09-19 08:27:59 +0200

    build

--- build/index.js
+++ build/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const command_line_1 = require("./infrastructure/command-line");
-const app_1 = require("./app/app");
-app_1.app.run((0, command_line_1.commandLine)(process));
+const http_server_1 = require("./infrastructure/http-server");
+const rot13_server_1 = require("./app/rot13-server");
+(0, rot13_server_1.app)((0, command_line_1.commandLine)(process), http_server_1.httpServer.create()).startAsync();
