2b5df707a4f43e888ee37f5cf855416924455d5f
cgombauld <clement.gombauld@cpexterne.org>
2022-09-23 21:52:24 +0200

    filled index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import { clock } from "./infrastructure/clock";
+import { commandLine } from "./infrastructure/command-line";
+import { go } from "./clock-exemple/clock-exemple";
 
+go(commandLine(process), clock.create());
