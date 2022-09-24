9b24565e9ed4fbd7014754d01c0d18fd4dbeb021
cgombauld <clement.gombauld@cpexterne.org>
2022-09-24 19:02:50 +0200

    clock add to the project

---
+++ src/infrastructure/__tests__/clock.test.ts
@@ -0,0 +1,147 @@
+import { clock } from "../clock";
+
+describe("clock", () => {
+  it("provides current timestamp", () => {
+    const realClock = clock.create();
+    expect(realClock.now()).toBeLessThanOrEqual(Date.now());
+  });
+
+  it("waits N miliseconds", async () => {
+    const realClock = clock.create();
+    const startTime = realClock.now();
+    const expectedTime = 10;
+    await realClock.waitAsync(expectedTime);
+    const elapsedTime = realClock.now() - startTime;
+    expect(expectedTime).toBeLessThanOrEqual(elapsedTime + 1);
+  });
+
+  it("fails fast when we use advanceNullAsync in production mode", () => {
+    const realClock = clock.create();
+
+    expect(
+      async () => await realClock.advanceNullAsync(0)
+    ).rejects.toThrowError("this method should not be use on real clock");
+  });
+
+  describe("nullability", () => {
+    it("defaults now to 0 miliseconds", () => {
+      const fakeClock = clock.createNull();
+      expect(fakeClock.now()).toBe(0);
+    });
+
+    it("should be configurable for now", () => {
+      const fakeClock = clock.createNull({ now: 42 });
+      expect(fakeClock.now()).toBe(42);
+    });
+
+    it("can advance the clock ", async () => {
+      const fakeClock = clock.createNull();
+      await fakeClock.advanceNullAsync(10);
+      expect(fakeClock.now()).toBe(10);
+    });
+
+    it("can wait", async () => {
+      const fakeClock = clock.createNull();
+      let wait: number | string = "waiting";
+      fakeClock.waitAsync(10).then(() => {
+        wait = fakeClock.now();
+      });
+      expect(wait).toBe("waiting");
+      await fakeClock.advanceNullAsync(20);
+      expect(wait).toBe(10);
+    });
+  });
+});
+
+describe("local Date", () => {
+  const checkFormattedString = (
+    format: Record<string, string>,
+    local?: string
+  ) => {
+    const realClock = clock.create();
+    let expectedTime = new Date().toLocaleString(local, format);
+    const formattedDate = realClock.toFormattedString(format, "fr");
+
+    if (formattedDate !== expectedTime) {
+      expectedTime = new Date().toLocaleString("fr", format);
+    }
+    expect(formattedDate).toEqual(expectedTime);
+  };
+
+  it("outputs time using computer language and computer time zone", () => {
+    const format: Record<string, string> = {
+      dateStyle: "medium",
+      timeStyle: "short",
+    };
+
+    checkFormattedString(format);
+  });
+
+  it("outputs the current time using the configured time zone the local", () => {
+    const format: Record<string, string> = {
+      timeZone: "Europe/Paris",
+      timeStyle: "short",
+    };
+
+    const local = "fr";
+
+    checkFormattedString(format, local);
+  });
+
+  describe("nullability", () => {
+    it("renders to formatted string", () => {
+      const format: Record<string, string> = {
+        timeZone: "Europe/Paris",
+        dateStyle: "medium",
+        timeStyle: "short",
+      };
+
+      const fakeClock = clock.createNull({ now: 0 });
+
+      expect(fakeClock.toFormattedString(format, "fr")).toBe(
+        "1 janv. 1970, 01:00"
+      );
+    });
+
+    it("defaults the time zone to GMT", () => {
+      const format: Record<string, string> = {
+        timeStyle: "long",
+      };
+
+      const fakeClock = clock.createNull({ now: 0 });
+
+      expect(fakeClock.toFormattedString(format, "en-US")).toBe(
+        "12:00:00 AM UTC"
+      );
+      expect(format).toEqual({
+        timeStyle: "long",
+      });
+    });
+
+    it("defaults locale to fr", () => {
+      const format: Record<string, string> = {
+        dateStyle: "medium",
+        timeStyle: "long",
+      };
+
+      const fakeClock = clock.createNull({ now: 0 });
+      expect(fakeClock.toFormattedString(format)).toBe(
+        "1 janv. 1970, 00:00:00 UTC"
+      );
+    });
+
+    it("allows local time zone and locale to be configured", () => {
+      const format: Record<string, string> = {
+        timeStyle: "long",
+      };
+
+      const fakeClock = clock.createNull({
+        now: 0,
+        timeZone: "America/New_York",
+        locale: "en-US",
+      });
+
+      expect(fakeClock.toFormattedString(format)).toBe("7:00:00 PM EST");
+    });
+  });
+});
---
+++ src/infrastructure/clock.ts
@@ -0,0 +1,94 @@
+import { pipe } from "ramda";
+import FakeTimers from "@sinonjs/fake-timers";
+import { withConstructor } from "../utils/withConstructor";
+
+export interface Clock {
+  Date: DateConstructor;
+  waitAsync: (miliseconds: number) => Promise<unknown>;
+  advanceNullAsync: (miliseconds: number) => Promise<void>;
+  toFormattedString: (
+    format: Record<string, string>,
+    locale?: string
+  ) => string;
+}
+
+interface ClockDependancy {
+  Date: DateConstructor;
+  DateTimeFormat: any;
+  setTimeout: (callback: () => Promise<unknown>, arg1?: number) => void;
+  advanceNullAsync?: (milliseconds: number) => Promise<number>;
+}
+
+const withClock =
+  ({
+    Date,
+    DateTimeFormat,
+    setTimeout,
+    advanceNullAsync = async (miliseconds: number) => {
+      throw new Error("this method should not be use on real clock");
+    },
+  }: ClockDependancy) =>
+  (o: any) => ({
+    ...o,
+    now: () => Date.now(),
+
+    waitAsync: async (miliseconds: number) =>
+      await new Promise((resolve) =>
+        setTimeout(async () => resolve("end of the timer"), miliseconds)
+      ),
+    advanceNullAsync: async (miliseconds: number) => {
+      await advanceNullAsync(miliseconds);
+    },
+    toFormattedString: (format: Record<string, string>, locale?: string) => {
+      const formatter = DateTimeFormat(locale, format);
+      return formatter.format(Date.now());
+    },
+  });
+
+const nullGlobals = (time = 0, configurableLocal = "fr", timeZone = "UTC") => {
+  const fake = FakeTimers.createClock(time);
+
+  return {
+    Date: fake.Date,
+    DateTimeFormat: (locale?: string, options?: Record<string, string>) => {
+      if (locale === undefined) {
+        locale = configurableLocal;
+      }
+      if (options && options.timeZone === undefined) {
+        options = { ...options, timeZone };
+      }
+      return Intl.DateTimeFormat(locale, options);
+    },
+    setTimeout: async (fn: () => Promise<unknown>, arg1 = 0) => {
+      fake.setTimeout(fn, arg1);
+    },
+    advanceNullAsync: async (milliseconds: number) =>
+      fake.tickAsync(milliseconds),
+  };
+};
+
+interface ClockInfraBuilder {
+  create: () => any;
+  createNull: (params: NullConfiguartion) => any;
+}
+
+interface NullConfiguartion {
+  now?: number;
+  locale?: string;
+  timeZone?: string;
+}
+
+const createClock = (
+  depedancy: ClockDependancy,
+  clockInfraBuilder: ClockInfraBuilder
+) => pipe(withClock(depedancy), withConstructor(clockInfraBuilder))({});
+
+export const clock = {
+  create: () =>
+    createClock(
+      { Date, DateTimeFormat: Intl.DateTimeFormat, setTimeout },
+      clock
+    ),
+  createNull: ({ now, locale, timeZone }: NullConfiguartion = {}) =>
+    createClock({ ...nullGlobals(now, locale, timeZone) }, clock),
+};
